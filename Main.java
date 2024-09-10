8671c58be2a94e23dcd4430abf31691cd8b195e3
Neil John <kazukiuchiha574@gmail.com>
2024-09-10 10:33:44 +0800

    Insertion of subtraction method in Java

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main {
     public int add (int a, int b){
         return a + b;
     }
+    public int subtract (int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Main calculator = new Main();
